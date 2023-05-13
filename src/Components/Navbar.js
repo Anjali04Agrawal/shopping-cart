6117f386a534aa3fe176c5d2ca1329a17b52afc6
Anjali Agrawal <79005886+Anjali04Agrawal@users.noreply.github.com>
2023-05-14 00:56:22 +0530

    Add files via upload
    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    state = {  } 
+    render() { 
+        return ( <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Apna Cart</a>
+        </div>
+      </nav>
+    );
+    }
+}
+ 
+export default Navbar;
---
+++ src/Components/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Product from './Product.js'
+
+export default function ProductList(props) {
+  return (
+    props.productList.length > 0 ?
+    props.productList.map((product, i)=>{
+        return <Product product = {product} key = {i} incrementQuantity = {props.incrementQuantity} index={i} decrementQuantity = {props.decrementQuantity} removeItem = {props.removeItem}/> 
+    })
+    : <h1> No Products exist in the cart.</h1>
+  )
+}
